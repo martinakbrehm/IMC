bed7ff13879e4ec51fc2b14e7c870a64429fc326
marti <martinaklippelbrehm@gmail.com>
2023-12-29 22:15:25 -0300

    add alert box

---
+++ js/script.js
@@ -0,0 +1,32 @@
+import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+import { IMC, notANumber } from "./utils.js"
+
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if (showAlertError) {
+        AlertError.open()
+        return;
+    
+    }
+    AlertError.close()
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.open()
+
+    Modal.message.innerText = message   
+
+}
+
