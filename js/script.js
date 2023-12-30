e286392315958f4f23050bcecc0ee68a19d87075
marti <martinaklippelbrehm@gmail.com>
2023-12-29 22:22:59 -0300

    function refactoring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from "./alert-error.js"
-import { IMC, notANumber } from "./utils.js"
+import { calculateIMC, notANumber } from "./utils.js"
 
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
@@ -12,21 +12,26 @@ form.onsubmit = function(event) {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if (showAlertError) {
+    if (weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     
     }
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)  
+
+}
+
+function displayResultMessage(result) {
     const message = `Seu IMC é de ${result}`
 
     Modal.open()
 
-    Modal.message.innerText = message   
-
+    Modal.message.innerText = message  
+    
 }
 
--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,6 @@ export function notANumber(value) {
 }
 
 
-export function IMC(weight, height) {
+export function calculateIMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
 }
