98fe0206be7d4b859d4d3d82e4f766c21248b41e
marti <martinaklippelbrehm@gmail.com>
2023-12-30 11:49:55 -0300

    add oninput

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
+inputHeight.oninput = () => AlertError.close()
+inputWeight.oninput = () => AlertError.close()
 
 form.onsubmit = function(event) {
     event.preventDefault()
